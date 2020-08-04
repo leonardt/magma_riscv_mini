d6dcc09e5e71c2f1d8ef88385d566a5219fef129
Lenny Truong <lenny@stanford.edu>
2020-08-04 11:39:25 -0700

    Use mux op

--- riscv_mini/reg_file.py
+++ riscv_mini/reg_file.py
@@ -21,10 +21,9 @@ class RegFile(m.Generator2):
             wdata=m.In(m.UInt[DATAWIDTH])
         ) + m.ClockIO(has_async_reset=True)
         regs = RegFileBuilder("reg_file", 32, DATAWIDTH, write_forward=False)
-        RegMux = m.Mux(2, m.UInt[DATAWIDTH])
-        io.rdata1 @= RegMux()(0, regs[io.raddr1], m.reduce(operator.or_,
-                                                           io.raddr1))
-        io.rdata2 @= RegMux()(0, regs[io.raddr2], m.reduce(operator.or_,
-                                                           io.raddr2))
+        io.rdata1 @= m.mux([0, regs[io.raddr1]], m.reduce(operator.or_,
+                                                          io.raddr1))
+        io.rdata2 @= m.mux([0, regs[io.raddr2]], m.reduce(operator.or_,
+                                                          io.raddr2))
         wen = m.bit(io.wen) & m.reduce(operator.or_, io.waddr)
         regs.write(io.waddr, io.wdata, enable=wen)
