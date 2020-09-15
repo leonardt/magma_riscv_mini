b6149214d4cce720bd24ccc42009093ec7060aa4
Lenny Truong <lenny@stanford.edu>
2020-09-15 09:38:14 -0700

    Use m.concat

--- riscv_mini/csr_gen.py
+++ riscv_mini/csr_gen.py
@@ -10,15 +10,6 @@ import riscv_mini.control as Control
 from riscv_mini.data_path import Const
 
 
-def concat(*args):
-    x = args[0]
-    if isinstance(x, Bit):
-        x = BitVector[1](x)
-    if len(args) == 1:
-        return x
-    return x.concat(concat(*args[1:]))
-
-
 def Valid(T):
     return m.Product.from_fields(f"Valid[{T}]", {
         "bits": T,
@@ -74,10 +65,10 @@ class CSRGen(m.Generator2):
         instret = m.Register(m.UInt[x_len])()
         instreth = m.Register(m.UInt[x_len])()
 
-        mcpuid = concat(BV[2](0),  # RV32I
-                        BV[x_len - 28](0),
-                        BV[26](1 << (ord('I') - ord('A')) |  # Base ISA
-                               1 << (ord('U') - ord('A'))))  # User Mode
+        mcpuid = m.concat(BV[2](0),  # RV32I
+                          BV[x_len - 28](0),
+                          BV[26](1 << (ord('I') - ord('A')) |  # Base ISA
+                                 1 << (ord('U') - ord('A'))))  # User Mode
         mimpid = BV[x_len](0)
         mhartid = BV[x_len](0)
 
