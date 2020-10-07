64639ac28301c000937867439145ea2e0edb28f5
Lenny Truong <lenny@stanford.edu>
2020-10-07 10:46:29 -0700

    init cache/nasti

---
+++ riscv_mini/cache.py
@@ -0,0 +1,31 @@
+import magma as m
+from riscv_mini.nasti import make_NastiIO
+
+
+def make_CacheReq(x_len):
+    class CacheReq(m.Product):
+        addr = m.UInt[x_len]
+        data = m.UInt[x_len]
+        mask = m.UInt[x_len // 8]
+    return CacheReq
+
+
+def make_CacheResp(x_len):
+    class CacheResp(m.Product):
+        data = m.UInt[x_len]
+    return CacheResp
+
+
+def make_CacheIO(x_len):
+    class CacheIO(m.Product):
+        abort = m.In(m.Bit)
+        req = m.Out(m.Valid[make_CacheReq(x_len)])
+        resp = m.In(m.Valid[make_CacheResp(x_len)])
+    return CacheIO
+
+
+def make_CacheModuleIO(x_len):
+    class CacheModuleIO(m.Product):
+        cpu = make_CacheIO(x_len)
+        nasti = make_NastiIO(x_len)
+    return CacheModuleIO
