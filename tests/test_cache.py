438d4e47f72cdcfebe764d583f22b5311da28572
Lenny Truong <lenny@stanford.edu>
2020-10-21 10:08:18 -0700

    WIP GoldCache

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,9 +1,41 @@
 import magma as m
 
-from riscv_mini.cache import Cache
+from riscv_mini.nasti import make_NastiIO, NastiParameters
+from riscv_mini.cache import Cache, make_CacheResp, make_CacheReq
+
+
+class GoldCache(m.Generator2):
+    def __init__(self, x_len, n_ways: int, n_sets: int, b_bytes: int):
+        nasti_params = NastiParameters(data_bits=64, addr_bits=x_len,
+                                       id_bits=5)
+
+        self.io = m.IO(
+            req=m.Consumer(m.Decoupled[make_CacheReq(x_len)]),
+            resp=m.Producer(m.Decoupled[make_CacheResp(x_len)]),
+            nasti=make_NastiIO(nasti_params)
+        )
+        size = m.bitutils.clog2(nasti_params.x_data_bits)
+        b_bits = b_bytes << 3
+        b_len = m.bitutils.clog2(b_bytes)
+        s_len = m.bitutils.clog2(n_sets)
+        t_len = x_len - (s_len + b_len)
+        n_words = b_bits // x_len
+        w_bytes = x_len // 8
+        byte_offset_bits = m.bitutils.clog2(w_bytes)
+        nasti_params = NastiParameters(data_bits=64, addr_bits=x_len,
+                                       id_bits=5)
+        data_beats = b_bits // nasti_params.x_data_bits
+        length = data_beats - 1
+
+        data = m.Memory(n_sets, m.UInt[b_bits])()
+        tags = m.Memory(n_sets, m.UInt[t_len])()
+        v = m.Memory(n_sets, m.Bit)
+        d = m.Memory(n_sets, m.Bit)
 
 
 def test_cache():
     MyCache = Cache(32, 1, 256, 4 * (32 >> 3))
     m.compile("build/MyCache", MyCache, inline=True,
               drive_undriven=True, terminate_unused=True)
+
+    GoldCache(32, 1, 256, 4 * (32 >> 3))
